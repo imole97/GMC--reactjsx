17009494142ffc804670115bbe6287d28d7b6d1a
Imole97 <imoleadebanjo97@gmail.com>
2022-01-02 13:21:17 +0100

    jsx first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import myImage from './imageInSrc.jpg'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{border:'solid 1px black', maxWidth:'100vw'}}>
+          <h1 className="title red">Imoleayo Adebanjo</h1>
+
+          <div><img src={myImage} alt='foodimage' width='400' height='400'/></div>
+
+          <div><img src="/imageInPublic.jpg" alt='foodimage'/></div>
+      </div>
+
+      <video width="320" height="240" controls>
+        <source src="myVideo.mp4" type="video/mp4"/>
+      </video>
     </div>
   );
 }
